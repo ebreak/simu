a543cc649f011de793e91ac892e4c169e84b0b9a
Pokyux <pokyux@mail.ustc.edu.cn>
2024-06-13 15:44:29 +0800

    add obj kind

--- core/object.h
+++ core/object.h
@@ -7,9 +7,11 @@ class universe;
 #include "universe.h"
 
 struct object {
+  i64 kind; // > 0 for renderable, <= 0 for unrenderable
   universe *u;
   coordinate position;
-  object(universe *_u, coordinate _position): u(_u), position(_position) {}
+  object(universe *_u, i64 _kind, coordinate _position)
+    :u(_u), kind(_kind), position(_position) {}
   virtual void tick_action() = 0;
   u64 spawn(object *obj);
   void awake(u64 id);
