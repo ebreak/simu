8c811aa001e7b7de2dbad48d2690148dcd8a0bf0
Pokyux <pokyux@mail.ustc.edu.cn>
2024-06-13 17:59:46 +0800

    add telecom

---
+++ core/telecom/basic.h
@@ -0,0 +1,10 @@
+#ifndef _CORE_TELECOM_BASIC_H
+#define _CORE_TELECOM_BASIC_H
+
+#include <mocutils/type.h>
+
+struct basic {
+  u64 session;
+};
+
+#endif
---
+++ core/telecom/human_move.h
@@ -0,0 +1,13 @@
+#ifndef _CORE_TELECOM_HUMAN_MOVE_H
+#define _CORE_TELECOM_HUMAN_MOVE_H
+
+#include <mocutils/type.h>
+
+#include "basic.h"
+#include "coordinate.h"
+
+struct human_move: basic {
+  coordinate delta;
+};
+
+#endif
