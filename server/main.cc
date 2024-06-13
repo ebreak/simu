df2a04f764202b95f70fa1712ed4d5a5acd834c5
Pokyux <pokyux@mail.ustc.edu.cn>
2024-06-13 18:50:06 +0800

    server init user & pyrite

--- server/main.cc
+++ server/main.cc
@@ -1,12 +1,17 @@
 #include <universe.h>
-#include <object/human.h>
 
 #include <mocutils/log.h>
+#include <object/human.h>
+
+#include "net.h"
+#include "user.h"
 
 int main() {
+  init_user();
+  init_server();
   universe u;
   auto id = u.insert(new human(&u, coordinate(0, 0)));
   u.activate(id);
-  u.start(100);
+  u.start(100, false);
   return 0;
 }
