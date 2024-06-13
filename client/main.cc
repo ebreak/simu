72e12becd8b18dd72b8aea687156ffed8ee9431e
Pokyux <pokyux@mail.ustc.edu.cn>
2024-06-13 16:30:13 +0800

    run opengl

--- client/main.cc
+++ client/main.cc
@@ -6,13 +6,26 @@
 #include <object/human.h>
 #include <mocutils/clock.h>
 
+void draw_point(float x, float y) {
+  glPointSize(20.0f);
+  glBegin(GL_POINTS);
+  glColor3f(1, 0, 0);
+  glVertex2f(x, y);
+  glEnd();
+}
+
 void render(universe *u) {
   if (!glfwInit()) exit(-1);
   GLFWwindow *window = glfwCreateWindow(1024, 1024, "SIMU", NULL, NULL);
   glfwMakeContextCurrent(window);
   moc::clock start;
   while (!glfwWindowShouldClose(window)) {
+    glClearColor(0, 0, 0, 0);
+    glClear(GL_COLOR_BUFFER_BIT);
     auto ro_data = u->ro_obj();
+    for (auto obj: ro_data)
+      draw_point(obj->position.x, obj->position.y);
+    glfwSwapBuffers(window);
     glfwPollEvents();
     start.tick(100);
   }
--- core/object/human.cc
+++ core/object/human.cc
@@ -7,12 +7,12 @@ human::human(universe *u, coordinate _position): object(u, obj_human, _position)
 }
 
 void human::tick_action() {
-  if (this->u->tick - last_spawn < 100)
+  if (this->u->tick - last_spawn < 10)
     return;
   last_spawn = this->u->tick;
   u64 id = this->spawn(new human(
     this->u, 
-    coordinate(this->position.x + 1, this->position.y + 1)
+    coordinate(this->position.x + 0.5, this->position.y + 0.5)
   ));
   this->awake(id);
 }
