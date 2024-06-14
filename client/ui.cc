46ab2df37fbed6247344ffa653f0ddaa7d45b82a
pokyux <pokyux@mail.ustc.edu.cn>
2024-06-14 15:22:06 +0800

    update move

--- client/ui.cc
+++ client/ui.cc
@@ -8,20 +8,30 @@
 #include "game.h"
 #include "net.h"
 
-void process_key(GLFWwindow *window) {
-  coordinate delta;
-  if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-    delta = coordinate(0, 1.0/32);
-  if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-    delta = coordinate(-1.0/32, 0);
-  if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-    delta = coordinate(0, -1.0/32);
-  if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-    delta = coordinate(1.0/32, 0);
-  if (delta == coordinate()) return;
+void key_callback(
+  GLFWwindow *window, int key, 
+  int scancode, int action, int mods
+) {
+  vector velocity;
   prt::bytes data(&session, sizeof(session));
-  data += prt::bytes(&delta, sizeof(coordinate));
-  c->tell("human-move", data);
+  switch (action) {
+  case GLFW_PRESS:
+    if (key == GLFW_KEY_W)
+      velocity = vector(0, 1.0/32);
+    else if (key == GLFW_KEY_A)
+      velocity = vector(-1.0/32, 0);
+    else if (key == GLFW_KEY_S)
+      velocity = vector(0, -1.0/32);
+    else if (key == GLFW_KEY_D)
+      velocity = vector(1.0/32, 0);
+    data += prt::bytes(&velocity, sizeof(coordinate));
+    c->tell("human-move", data);
+    break;
+  case GLFW_RELEASE:
+    data += prt::bytes(&velocity, sizeof(coordinate));
+    c->tell("human-move", data);
+    break;
+  }
 }
 
 void render(GLFWwindow *window) {
@@ -36,13 +46,14 @@ void render(GLFWwindow *window) {
 void ui_mainloop() {
   while (!u) ;
 
+  // glfw init
   if (!glfwInit()) exit(-1);
   GLFWwindow *window = glfwCreateWindow(1024, 1024, "SIMU", NULL, NULL);
   glfwMakeContextCurrent(window);
+  glfwSetKeyCallback(window, key_callback);
 
   moc::clock start;
   while (!glfwWindowShouldClose(window)) {
-    process_key(window);
     render(window);
     glfwPollEvents();
     start.tick(10);
