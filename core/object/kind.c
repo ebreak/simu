d0d290de455c3ff537f4fd35ba18353c76481fa5
pokyux <pokyux@mail.ustc.edu.cn>
2024-06-14 14:38:25 +0800

    add kind of size

---
+++ core/object/kind.c
@@ -0,0 +1,7 @@
+#include "kind.h"
+
+#include <object.h>
+
+#include "human.h"
+
+u64 object_size[2] {sizeof(object), sizeof(human)};
--- core/object/kind.h
+++ core/object/kind.h
@@ -1,8 +1,12 @@
 #ifndef __CORE_OBJECT_KIND_H
 #define __CORE_OBJECT_KIND_H
 
+#include <mocutils/type.h>
+
 enum object_kind {
   obj_human = 1,
 };
+
+extern u64 object_size[2];
 
 #endif
