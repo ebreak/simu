d37a8d071caad73e56a4f9d65a8d2714c3025d61
pokyux <pokyux@mail.ustc.edu.cn>
2024-06-14 16:58:22 +0800

    impl vector

--- core/vector.h
+++ core/vector.h
@@ -1,8 +1,34 @@
 #ifndef __CORE_VECTOR_H
 #define __CORE_VECTOR_H
 
+#include <cmath>
+
 #include <coordinate.h>
 
-typedef coordinate vector;
+struct vector: coordinate {
+#ifdef SIMU_3D
+  double z;
+  vector(): x(0), y(0), z(0) {}
+  vector(double _x, double _y, double _z): x(_x), y(_y), z(_z) {}
+#else
+  vector(): x(0), y(0) {}
+  vector(double _x, double _y): x(_x), y(_y) {}
+#endif
+
+  vector(polar p) {
+    this->x = p.d * cos(p.angle);
+    this->y = p.d * sin(p.angle);
+  }
+
+  vector operator/(double d) {
+    vector ret = *this;
+    ret.x /= d;
+    ret.y /= d;
+#ifdef SIMU_3D
+    ret.z /= d;
+#endif
+    return ret;
+  }
+};
 
 #endif
